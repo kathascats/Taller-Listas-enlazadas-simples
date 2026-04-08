aed59da022643ee9af5bb53ca98af4aeb6c14daf
kathascats <katilyp@uninorte.edu.co>
2026-04-08 15:58:06 +0000

    Agregar implementación de la clase Nodo y listaEnlazada con método para insertar al inicio

---
+++ ejercicios123.py
@@ -0,0 +1,25 @@
+# ejercicio 1
+class Nodo:
+    def __init__(self, dato):
+        self.dato = dato
+        self.siguiente = None
+        
+# ejercicio 2 y 3
+from nodo import Nodo
+
+class listaEnlazada:
+    def __init__(self):
+        self.head = None
+
+def insertar_inicio(self, dato):
+nuevo = Nodo(dato)
+nuevo.siguiente = self.head
+self.head = nuevo
+
+
+
+
+
+
+
+
