b14e1596571c8b27bd46de8a07da454d87cb39e7
kathascats <katilyp@uninorte.edu.co>
2026-04-13 07:28:44 -0500

    main hasta ej 4 agregado
    Updated import statement and modified print formatting. Added new functionality for insertion and search operations in the linked list.

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-from lista_enlazada import ListaEnlazada
+from ejercicios123 import ListaEnlazada
 
+# ejercicio 3
+print("\n Inserción")
 lista = ListaEnlazada()
+lista.insertar_final(20)
+lista.insertar_final(30)
+lista.insertar_iniciio(10)
 
-lista.insertar_inicio(20)
-lista.insertar_inicio(10)
-lista.insertar_inicio(30)
-
-# recorrido
-actual = lista.cabeza
+actual = lista.head
 while actual is not None:
-    print(actual.dato, end= " - ")
+    print(actual.dato, end=" ->")
     actual = actual.siguiente
 print("null")
+
+# ejercicio 4
+print("\n Busqueda")
+print(f"Buscar 20 - posicion: {lista.tamaño()}")
+
